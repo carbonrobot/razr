d205896b2cd9d9d14d311c6b0d8ffa58b7105279
Charles Brown <carbonrobot@gmail.com>
2012-04-29 00:06:54 -0500

    Added unit testing

---
+++ Razr.Tests/Models/PostTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Razr.Models;
+
+namespace Razr.Tests.Models
+{
+    [TestClass]
+    public class PostTests
+    {
+        [TestMethod]
+        public void CanCreateNewPosts()
+        {
+            var p = new Post();
+            p.Summary = "New post";
+            p.Body = "I am always dancing in the moonlight";
+        }
+    }
+}
