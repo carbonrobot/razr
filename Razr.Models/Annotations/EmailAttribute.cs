01b6cfec74bd04bda87ac4079359573b7fdf72b2
cb <carbonrobot@gmail.com>
2012-06-13 15:35:09 -0500

    Adding syntaxhighlighter

---
+++ Razr.Models/Annotations/EmailAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace Razr.Models.Annotations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Requires the string property to conform to an email address
+    /// </summary>
+    public class EmailAttribute : RegularExpressionAttribute
+    {
+        public EmailAttribute() : base("^[a-z0-9_\\+-]+(\\.[a-z0-9_\\+-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*\\.([a-z]{2,4})$") { }
+    }
+}
