01384fa5006e4414c222b5c87bbffdc776ed9e64
Charlie Brown <carbonrobot@gmail.com>
2012-07-27 09:22:52 -0500

    Add google prettify for syntax highlighting

--- Razr.Web/Components/Markdown/RazrMarkdown.cs
+++ Razr.Web/Components/Markdown/RazrMarkdown.cs
@@ -20,62 +20,6 @@ namespace Razr.Web.Components.Markdown
         {
             return _markdown.Transform(markdownText);
         }
-                
-
-        /*
-        private MarkdownSharp.Markdown _markdown;
-        private IEnumerable<Transformer> _tranformers;
-
-        public RazrMarkdown()
-        {
-            _markdown = new MarkdownSharp.Markdown(new MarkdownOptions
-            {
-                AutoHyperlink = true,
-                AutoNewLines = false,
-                EncodeProblemUrlCharacters = true,
-                LinkEmails = true
-            });
-            _tranformers = Transformer.GetAll();
-        }
-
-        public string Transform(string markdownText)
-        {
-            //foreach (var transformer in _tranformers)
-                //markdownText = transformer.Transform(markdownText);
-            
-            return _markdown.Transform(markdownText);
-        }
-         */
-
-        /*
-        public RazrMarkdown() { 
-            _markdown = new MarkdownSharp.Markdown(createMarkdownOptions());
-			_tranformers = new Tranformers();
-        }
-
-        private readonly ITranformers _tranformers;
-        private readonly MarkdownSharp.Markdown _markdown;
-
-        
-        public string Transform(string markdownText)
-        {
-            foreach (var preTransformation in _tranformers.GetTransformers())
-                markdownText = preTransformation.Invoke(markdownText);
-
-            return _markdown.Transform(markdownText);
-        }
-
-        private MarkdownOptions createMarkdownOptions()
-        {
-            return new MarkdownOptions
-            {
-                AutoHyperlink = true,
-                AutoNewLines = false,
-                EncodeProblemUrlCharacters = true,
-                LinkEmails = true
-            };
-        }
-         */
 
     }
 }
---
+++ Razr.Web/Scripts/razr/autoloader.js
@@ -0,0 +1,23 @@
+﻿// Razr autoloader
+var Razr = (function ($, window, document, undefined) {
+
+    // on ready, execute
+    $(function () {
+        Razr.init();
+    });
+
+    var setupHighlighting = function () {
+        // add pretty print required classes to code elements
+        $('pre').addClass('prettyprint');
+
+        // enable pretty print
+        prettyPrint();
+    };
+
+    return {
+        init: function () {
+            setupHighlighting();
+        }
+    };
+
+})(jQuery, window, document);
